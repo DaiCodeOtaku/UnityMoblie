9a3fbfec4269a13ff73c56e1abdc1ae8ebf4b937
Scintoth <Scott93Davis@gmail.com>
2015-01-21 18:55:50 +0000

    Re-commit movement

--- ProjectIR/Assets/PlayerControls.cs
+++ ProjectIR/Assets/PlayerControls.cs
@@ -5,9 +5,11 @@ using System;
 public class PlayerControls : MonoBehaviour 
 {
 	public float speed;
+	enum scheme {moveScale, tilt, arrows, accel, SOD, moveScaleInv, tiltInv, arrowsInv, accelInv, SODInv};
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 	}
 	
 	// Update is called once per frame
@@ -16,25 +18,49 @@ public class PlayerControls : MonoBehaviour
 
 		Vector3 mousePos = new Vector3 (0, 0, 0);
 
+		switch (scheme)
+		{
+		case scheme.moveScale:
+				if (Input.GetMouseButton (0) && Input.mousePosition.x > (Screen.width / 2)) 
+				{	
+					mousePos.x = Input.mousePosition.x;
+					speed = 0.05f * 0.75f;
+					if (this.transform.position.x + 0.25 < 3.1) 
+					{
+						this.transform.Translate ((mousePos.x - (Screen.width / 2)) * speed * Time.deltaTime, 0, 0);
+					}
+				} 
+				else if (Input.GetMouseButton (0) && Input.mousePosition.x < (Screen.width / 2)) 
+				{	
+					mousePos.x = Input.mousePosition.x;
+					speed = -0.05f * 0.75f;
+					if (this.transform.position.x - 0.25 > -3.1) 
+					{
+						this.transform.Translate ((((Screen.width / 2) - mousePos.x) * speed * Time.deltaTime), 0, 0);
+					}
+				}
+				break;
+		case scheme.moveScaleInv:
+			if (Input.GetMouseButton (0) && Input.mousePosition.x > (Screen.width / 2)) 
+			{	
+				mousePos.x = Input.mousePosition.x;
+				speed = -0.05f * 0.75f;
+				if (this.transform.position.x + 0.25 < 3.1) 
+				{
+					this.transform.Translate ((mousePos.x - (Screen.width / 2)) * speed * Time.deltaTime, 0, 0);
+				}
+			} 
+			else if (Input.GetMouseButton (0) && Input.mousePosition.x < (Screen.width / 2)) 
+			{	
+				mousePos.x = Input.mousePosition.x;
+				speed = 0.05f * 0.75f;
+				if (this.transform.position.x - 0.25 > -3.1) 
+				{
+					this.transform.Translate ((mousePos.x - (Screen.width / 2)) * speed * Time.deltaTime, 0, 0);
+				}
+			}
+		//Move movement schemes go here
 
-		if (Input.GetMouseButton (0) && Input.mousePosition.x > (Screen.width / 2)) 
-		{	
-			mousePos.x = Input.mousePosition.x;
-			speed = 0.05f * 0.75f;
-			if (this.transform.position.x + 0.25 < 3.1) 
-			{
-				this.transform.Translate ((mousePos.x - (Screen.width / 2)) * speed * Time.deltaTime, 0, 0);
-			}
-		} 
-		else if (Input.GetMouseButton (0) && Input.mousePosition.x < (Screen.width / 2)) 
-		{	
-			mousePos.x = Input.mousePosition.x;
-			speed = -0.05f * 0.75f;
-			if (this.transform.position.x - 0.25 > -3.1) 
-			{
-			this.transform.Translate ((((Screen.width / 2) - mousePos.x) * speed * Time.deltaTime), 0, 0);
-			}
 		}
-		
 	}
 }
