a71090af850b54fa69fbc7ee41bd26e3264310a5
L9Aug <tbampton@hotmail.co.uk>
2015-01-21 15:41:16 +0000

    Fixed Scene Overwrite

---
+++ ProjectIR/Assets/Resources/Scripts/ObstacleController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleController : MonoBehaviour {
+
+	public static GameObject ObstacleTemplate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(spawnObstacle()){
+			GameObject Obstacle = new GameObject();
+			Obstacle = ObstacleTemplate;
+			Instantiate(Obstacle);
+		}
+	}
+
+	bool spawnObstacle(){
+		if ((Random.value * 1000.0f) <= 10) {
+			return true;
+		}
+		return false;
+	}
+}
