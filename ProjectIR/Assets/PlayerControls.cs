f7b12eddf27aa0b31b8cb63fbf5385365dec48e6
Scintoth <Scott93Davis@gmail.com>
2015-01-21 18:51:27 +0000

    Movement Basics
    Movement "slider" type, with the Inverse in there too.

---
+++ ProjectIR/Assets/PlayerControls.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class PlayerControls : MonoBehaviour 
+{
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		Vector3 mousePos = new Vector3 (0, 0, 0);
+
+
+		if (Input.GetMouseButton (0) && Input.mousePosition.x > (Screen.width / 2)) 
+		{	
+			mousePos.x = Input.mousePosition.x;
+			speed = 0.05f * 0.75f;
+			if (this.transform.position.x + 0.25 < 3.1) 
+			{
+				this.transform.Translate ((mousePos.x - (Screen.width / 2)) * speed * Time.deltaTime, 0, 0);
+			}
+		} 
+		else if (Input.GetMouseButton (0) && Input.mousePosition.x < (Screen.width / 2)) 
+		{	
+			mousePos.x = Input.mousePosition.x;
+			speed = -0.05f * 0.75f;
+			if (this.transform.position.x - 0.25 > -3.1) 
+			{
+			this.transform.Translate ((((Screen.width / 2) - mousePos.x) * speed * Time.deltaTime), 0, 0);
+			}
+		}
+		
+	}
+}
