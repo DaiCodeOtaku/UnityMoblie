f141826091958d94521d9ac408f5988066b7e46f
Oliver Thauerer <up698897@myport.ac.uk>
2015-01-21 12:50:55 +0000

    upload project

---
+++ project/Assets/StartGui.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+
+public class StartGui : MonoBehaviour {
+	public Texture btnTexture;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+
+	}
+
+	void OnGUI() {
+				if (!btnTexture) {
+						Debug.LogError ("Please assign a texture on the inspector");
+						return;
+				}
+				if (GUI.Button (new Rect (300, 500, 400, 200), btnTexture))
+						Debug.Log ("Clicked the button with an image");
+				if (GUI.Button (new Rect (300, 400, 100, 30), "Click"))
+						Debug.Log ("Clicked the button with text");
+
+		}
+
+}
+
