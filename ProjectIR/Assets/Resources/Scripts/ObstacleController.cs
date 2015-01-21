9b70a8ae30eaea8965277067db478a2ea4bdb1d8
L9Aug <tbampton@hotmail.co.uk>
2015-01-21 18:51:36 +0000

    Random Generation implemented
    good stuffs

--- ProjectIR/Assets/Resources/Scripts/ObstacleController.cs
+++ ProjectIR/Assets/Resources/Scripts/ObstacleController.cs
@@ -3,26 +3,60 @@ using System.Collections;
 
 public class ObstacleController : MonoBehaviour {
 
-	public static GameObject ObstacleTemplate;
+	public GameObject ObstacleTemplate;
+	enum spawnPatterns {Basic, Advanced};
+	float patternTimer;
 
 	// Use this for initialization
 	void Start () {
-		
+		patternTimer = 5;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		patternTimer -= Time.deltaTime;
+		if(patternTimer < 0){
+			newPattern((spawnPatterns)1);
+		}
 		if(spawnObstacle()){
-			GameObject Obstacle = new GameObject();
-			Obstacle = ObstacleTemplate;
-			Instantiate(Obstacle);
+			//Random.Range(-3.1f, 3.1f);
+			//ObstacleTemplate.transform.Translate(new Vector3(10, 0, 0));
+			ObstacleTemplate.transform.Translate(new Vector3(Random.Range(-3.1f, 3.1f), 7, -0.5f));
+			GameObject.Instantiate((GameObject)ObstacleTemplate);
+			resetObstacle();
 		}
 	}
 
 	bool spawnObstacle(){
-		if ((Random.value * 1000.0f) <= 10) {
+		if ((Random.value * 1000.0f) <= 100) {
 			return true;
 		}
 		return false;
 	}
+
+	spawnPatterns newPattern(spawnPatterns previousPattern){
+		int nextPattern = (int)((2 * Random.value)+1);
+		switch(nextPattern){
+		case 0:
+			if(nextPattern == (int)previousPattern){
+				return (spawnPatterns)1;
+			}
+			return 0;
+
+		case 1:
+			if(nextPattern == (int)previousPattern){
+				return 0;
+			}
+			return (spawnPatterns)1;
+
+		}
+		return 0;
+	}
+
+	void resetObstacle(){
+		ObstacleTemplate.transform.Translate(-ObstacleTemplate.transform.position);
+	}
+
+
+
 }
