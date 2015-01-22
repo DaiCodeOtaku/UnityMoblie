8bbefcb1232855228b486b93a991d9a8012aa32b
Q <marq46.steam@googlemail.com>
2015-01-22 00:41:30 +0000

    Minor assets

---
+++ ProjectIR/Assets/Resources/Scripts/simpleSpin.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simpleSpin : MonoBehaviour {
+	float rot = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rot < 360) {
+			rot += 1;
+		}
+		else {
+			rot = 0;
+		}
+
+		this.transform.Rotate(new Vector3(0.0f, 0.0f, 1.0f));
+	}
+}
