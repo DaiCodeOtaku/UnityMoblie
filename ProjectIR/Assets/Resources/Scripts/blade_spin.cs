beacf6ae7d33650021412b449fc76f3238732a8d
Q <marq46.steam@googlemail.com>
2015-02-18 21:36:54 +0000

    dud3 g3t m0d3ls br0

--- ProjectIR/Assets/Resources/Scripts/blade_spin.cs
+++ ProjectIR/Assets/Resources/Scripts/blade_spin.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class simpleSpin : MonoBehaviour {
+public class blade_spin : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f));
+		this.transform.Rotate(new Vector3(0.0f, -16.0f, 0.0f));
 	}
 }
