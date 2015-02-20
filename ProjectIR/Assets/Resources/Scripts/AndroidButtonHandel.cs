410b2f2871a73e011bb3535a18027a9b54e4f9c8
Oliver Thauerer <up698897@myport.ac.uk>
2015-02-20 11:20:48 +0000

    update

---
+++ ProjectIR/Assets/Resources/Scripts/AndroidButtonHandel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AndroidButtonHandel : MonoBehaviour {
+
+
+
+   public GameObject G;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+	
+		if(Input.GetKey(KeyCode.Escape))
+		{
+            G.SetActive(true);
+            //QuitCon Quit = (QuitCon)GameObject.FindObjectOfType<QuitCon>();
+           // Quit.Show();
+		};
+
+
+	}
+}
---
+++ ProjectIR/Assets/Resources/Scripts/UiScore.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UiScore : MonoBehaviour {
+
+    public Text tex;
+    string Score;
+    public int VarScore;
+	// Use this for initialization
+
+
+	void Start () 
+    {
+      
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+
+
+
+       ScoreController S = (ScoreController)GameObject.FindObjectOfType<ScoreController>();
+       VarScore = (int)S.score;
+
+        Score = VarScore.ToString();
+        tex.text = Score;
+	}
+
+    public void Hide()
+    {
+       gameObject.SetActive(false);
+    }
+}
